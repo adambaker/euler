f2e66bca872bfc94aac97468d5b2a3e93da512d2
Adam C. Baker <adam.durandal@gmail.com>
2012-09-24 17:02:04 -0700

    move factors into general module.

--- Euler.hs
+++ Euler.hs
@@ -1,4 +1,23 @@
-module Euler where
+module Euler (
+  factors,
+  divides,
+) where
+
+import Data.List (find)
+import Test.QuickCheck
+
+factors :: Integral i => i -> [i]
+factors n = let
+    factors' _ 1 = []
+    factors' start i = case find (`divides` i) [start..maxPossible] of
+      Nothing -> [i]
+      Just d  -> d : factors' d (i `div` d)
+      where
+        maxPossible = floor $ sqrt (fromIntegral i)
+  in factors' 2 n
+
+prop_product_of_factors :: Int -> Property
+prop_product_of_factors i = (i > 1) ==> foldl (*) 1 (factors i) == i
 
 divides :: Integral a => a -> a -> Bool
 d `divides`  k = k `mod` d == 0
